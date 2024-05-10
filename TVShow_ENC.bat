bc42f82da876284a824a8220eaefeebaff49c58f
o0cynix0o <o0cynix0o@gmail.com>
2024-05-10 14:13:16 -0600

    adding to repo

---
+++ TVShow_ENC.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM Check if a folder was dropped onto the batch file
+if "%~1" == "" (
+    echo Please drag and drop a folder onto this batch file.
+    pause
+    exit /b
+)
+
+REM Run the PowerShell script with the dropped folder as a parameter
+powershell.exe -ExecutionPolicy Bypass -File "G:\Scripts\Working\TVShowENC.ps1" "%~1"
+
+pause
