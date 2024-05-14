f8eda60295f1e077592c7046631efd58c9cae60c
o0cynix0o <o0cynix0o@gmail.com>
2024-05-14 15:23:10 -0600

    added some stuff

---
+++ Movie_ENC2.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM Check if a folder was dropped onto the batch file
+if "%~1" == "" (
+    echo Please drag and drop a folder onto this batch file.
+    pause
+    exit /b
+)
+
+REM Run the MovieENC PowerShell script with the dropped folder as a parameter
+powershell.exe -ExecutionPolicy Bypass -File "G:\VSCode\SABnzbPPS\PostProcessX265 - PSTest.ps1" "%~1"
+
+Rem pause << Editied out to see if the script still pauses
